f8a24bcc8e2075c83ac2e82d7ca1d86b588bdf4b
diyagaur10 <146921988+diyagaur10@users.noreply.github.com>
2024-02-25 11:22:19 +0530

    Update ml_&_tkinter.py

--- ml_&_tkinter.py
+++ ml_&_tkinter.py
@@ -75,31 +75,26 @@ def predict_url(url):
     result = logit.predict(X_predict)
     return result[0]
 
-# Create main window
 root = tk.Tk()
 root.title("Defence Cops")
 root.configure(bg="black")
 
-# Set window dimensions and position
 window_width = root.winfo_screenwidth()
 window_height = root.winfo_screenheight()
 root.geometry(f"{window_width}x{window_height}+0+0")
 
-# Load and configure the background image
 bg_image = Image.open("another.jpg")
 bg_image = bg_image.resize((window_width, window_height))
 bg_photo = ImageTk.PhotoImage(bg_image)
 bg_label = tk.Label(root, image=bg_photo)
 bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-# Create and place widgets
 heading_label = tk.Label(root, text="Defence Cops", font=("Helvetica", 38), fg="light green", bg="black")
 heading_label.pack(pady=20)
 
 # Load the machine learning model 
 vectorizer, logit = load_model()
 
-# Create a frame for the "Enter URL" section and center it
 url_frame = tk.Frame(root, bg="black")
 url_frame.pack(expand=True)
 
@@ -109,7 +104,6 @@ label.pack(pady=10)
 url_entry = tk.Entry(url_frame, width=50, font=("Helvetica", 14), bg="light green", fg="black")
 url_entry.pack(pady=10)
 
-# Add a button to manually trigger URL checking
 check_button = tk.Button(root, text="Check URL", command=check_url, font=("Helvetica", 16), bg="light green", fg="black")
 check_button.pack(pady=10)
 
@@ -123,7 +117,7 @@ def is_ssl_certified(url):
         else:
             return False
     except urllib3.exceptions.MaxRetryError:
-        # Handle MaxRetryError (e.g., when the server refuses connections)
+        
         return False
     except Exception as e:
         print(f"Error: {e}")
@@ -145,21 +139,21 @@ def check_ssl_certification():
 ssl_button = tk.Button(root, text="Check SSL Certification", command=check_ssl_certification, font=("Helvetica", 16), bg="light green", fg="black")
 ssl_button.pack(pady=10)
 
-def select_image():
+'''def select_image():
     file_path = filedialog.askopenfilename(title="Select Background Image", filetypes=[("Image files", ".png;.jpg;.jpeg;.gif")])
     if file_path:
-        set_background_image(file_path)
+        set_background_image(file_path)'''
 
 # Add a button to select a background image
 '''select_image_button = tk.Button(root, text="Select Background Image", command=select_image, font=("Helvetica", 14), bg="light green", fg="black")
 select_image_button.pack(pady=10)'''
 
-def set_background_image(another):
+'''def set_background_image(another):
     bg_image = Image.open(another)
     bg_image = bg_image.resize((window_width, window_height))
     bg_photo = ImageTk.PhotoImage(bg_image)
     bg_label.configure(image=bg_photo)
-    bg_label.image = bg_photo
+    bg_label.image = bg_photo'''
 
 # Run the Tkinter event loop
 root.mainloop()
